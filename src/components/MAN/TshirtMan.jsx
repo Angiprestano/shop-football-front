8e186e8f31ae94d0d79e3dbe1991e1552a18b0e9
Angela Prestano <angiprestano99@gmail.com>
2024-03-03 13:00:35 +0100

    footer tshirt

--- src/components/MAN/TshirtMan.jsx
+++ src/components/MAN/TshirtMan.jsx
@@ -1,9 +1,61 @@
 import { useDispatch, useSelector } from "react-redux";
 import { ActionTypes, getTshirtMan } from "../../Redux/action";
 import { useEffect, useState } from "react";
-import { Button, Card, Col, Container, ListGroup, Row } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Col,
+  Container,
+  ListGroup,
+  Nav,
+  Row,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { ArrowLeft } from "react-bootstrap-icons";
+import {
+  ArrowLeft,
+  Facebook,
+  Instagram,
+  Linkedin,
+  Snapchat,
+  Telegram,
+  Tiktok,
+  Twitch,
+  Whatsapp,
+  Youtube,
+} from "react-bootstrap-icons";
+import { BsTwitterX } from "react-icons/bs";
+import {
+  FaCcAmazonPay,
+  FaCcApplePay,
+  FaCcJcb,
+  FaCcMastercard,
+  FaCcPaypal,
+  FaCcVisa,
+  FaEbay,
+  FaGooglePay,
+} from "react-icons/fa";
+import {
+  SiAdidas,
+  SiCloud66,
+  SiCodesignal,
+  SiIveco,
+  SiJordan,
+  SiLogitech,
+  SiMaserati,
+  SiMercedes,
+  SiMetro,
+  SiMicrosoft,
+  SiMicrosoftazure,
+  SiMicrosoftbing,
+  SiNdr,
+  SiNear,
+  SiNike,
+  SiNrwl,
+  SiPuma,
+  SiQatarairways,
+  SiSimplenote,
+  SiSingaporeairlines,
+} from "react-icons/si";
 
 const TshirtMan = () => {
   const token = useSelector((state) => state.token);
@@ -99,6 +151,101 @@ const TshirtMan = () => {
             <p>Caricamento in corso...</p>
           )}
         </Row>
+      </Container>
+      <Container
+        fluid
+        className="footerInter bg bg-black text-white pt-3 ms-0 me-0"
+      >
+        <div className="sponsor  d-flex flex-wrap justify-content-center">
+          <SiNike />
+          <FaEbay className="ms-4" />
+          <SiQatarairways className="ms-4 mb-1" />
+          <SiAdidas className="ms-4" />
+          <SiCloud66 className="ms-4" />
+          <SiCodesignal className="ms-4" />
+          <SiIveco className="ms-4" />
+          <SiMercedes className="ms-4" />
+          <SiMicrosoftbing className="ms-4" />
+          <SiMicrosoftazure className="ms-4" />
+          <SiMetro className="ms-4" />
+          <SiLogitech className="ms-4" />
+          <SiNdr className="ms-4 mb-2" />
+          <SiNear className="ms-4" />
+          <SiSingaporeairlines className="ms-4" />
+          <SiNrwl className="ms-4" />
+          <SiSimplenote className="ms-4" />
+          <SiPuma className="ms-4" />
+          <SiJordan className="ms-4" />
+          <SiMaserati className="ms-4" />
+          <SiMicrosoft className="ms-4" />
+        </div>
+
+        <Nav className="d-flex flex-column flex-md-row justify-content-around">
+          <div className="textStyle d-flex flex-row mt-4 mt-3">
+            <span className="d-flex flex-column ms-4 ">
+              <h5>SHOPPING</h5>
+              <p>Acquisti online</p>
+              <p>Metodi di pagamento</p>
+              <p>My Account</p>
+              <p>Gift Card</p>
+              <p>WishList</p>
+              <p>Sconto Studenti</p>
+            </span>
+            <span className="d-flex flex-column ms-4 ps-4">
+              <h5>HELP</h5>
+              <p>Contattaci</p>
+              <p>FAQ</p>
+              <p>Traccia ordini e resi</p>
+              <p>Spedizioni</p>
+              <p>Resi e rimborsi</p>
+              <p>Guida alle taglie</p>
+            </span>
+            <span className="d-flex flex-column ms-4 ps-4 ">
+              <h5>AREA LEGALE</h5>
+              <p>Termini e Condizioni di Acquisto</p>
+              <p>Privacy Policy</p>
+              <p>Cookie Policy</p>
+            </span>
+            <span className="d-flex flex-column ms-4 ps-4 ">
+              <h5>STORES</h5>
+              <p>Inter Store Milano</p>
+              <p>Inter Store San Siro </p>
+              <p>Inter Store Castello</p>
+            </span>
+          </div>
+
+          <hr className="my-4 d-md-none"></hr>
+
+          <div className="icons d-flex justify-content-center justify-content-md-end align-items-center flex-wrap mt-3 mt-md-0 ">
+            <Telegram className="me-3" />
+            <Linkedin className="me-3" />
+            <Tiktok className="me-3" />
+            <Youtube className="me-3" />
+            <Twitch className="me-3" />
+            <Instagram className="me-3" />
+            <Snapchat className="me-3" />
+            <Facebook className="me-3" />
+            <Whatsapp className="me-3" />
+            <BsTwitterX />
+            <div className="cart d-flex flex-row">
+              <FaCcPaypal className="me-4" />
+              <FaCcMastercard className="me-4" />
+              <FaCcAmazonPay className="me-4" />
+              <FaCcApplePay className="me-4" />
+              <FaCcVisa className="me-4" />
+              <FaCcAmazonPay className="me-4" />
+              <FaGooglePay className="me-4" />
+              <FaCcJcb className="me-4" />
+            </div>
+          </div>
+          <hr className="my-4 d-md-none"></hr>
+          <div className="cookie text-center text-md-start">
+            <h6>IMPOSTAZIONI COOKIE</h6>
+            <p>
+              Copyright © 1995-2022 F.C. Internazionale Milano P.IVA 04231750151
+            </p>
+          </div>
+        </Nav>
       </Container>
     </div>
   );
